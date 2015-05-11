bf5005cefc9d0fcea98891e042edd7bd812424a2
Leon Mergen <leon@solatis.com>
2015-05-11 14:31:04 +0700

    Adds additional test case for OP_RETURN

--- test/Data/Bitcoin/ScriptSpec.hs
+++ test/Data/Bitcoin/ScriptSpec.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE TemplateHaskell   #-}
+
 module Data.Bitcoin.ScriptSpec where
 
 import           Data.Bitcoin.Script
 
+import qualified Data.ByteString.Char8      as BS8 (pack)
 import qualified Data.ByteString.Lazy.Char8 as BSL8 (pack)
 
 import           Test.Hspec
@@ -14,10 +17,16 @@ spec = do
 
       (encode . decode) hex `shouldBe` hex
 
-    it "succesfully parses a transaction into a meaningful object" $ do
+    it "succesfully parses a script into a meaningful object" $ do
       let hex     = BSL8.pack "76a914975efcba1e058667594dc57146022ec46560a63c88ac"
           decoded = decode hex
 
       case decoded of
        (Script [OP_DUP, OP_HASH160, OP_PUSHDATA _ OPCODE, OP_EQUALVERIFY, OP_CHECKSIG]) -> return ()
        _                 -> expectationFailure ("Result does not match expected: " ++ show decoded)
+
+    it "succesfully parses an OP_RETURN script into a meaningful object" $ do
+      let hex     = BSL8.pack "6a0b68656c6c6f20776f726c64"
+          decoded = decode hex
+
+      decoded `shouldBe` (Script [OP_RETURN, OP_PUSHDATA (BS8.pack "hello world") OPCODE])
